e9199dc4487753f271782438c00c55a2214facea
ale.iphone <ale.iphone@gmail.com>
2024-05-30 13:10:36 +0000

    simplify chart

--- pages/components/HeartrateChart.tsx
+++ pages/components/HeartrateChart.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { LineChart, Line, ResponsiveContainer } from 'recharts';
 
 interface HeartRateChartProps {
     data: { time: string; heartRate: number }[];
@@ -7,16 +7,13 @@ interface HeartRateChartProps {
 
 const HeartRateChart: React.FC<HeartRateChartProps> = ({ data }) => {
     return (
-        <ResponsiveContainer width="100%" height={300}>
-            <LineChart data={data}>
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="time" />
-                <YAxis />
-                <Tooltip />
-                <Legend />
-                <Line type="monotone" dataKey="heartRate" stroke="#8884d8" activeDot={{ r: 8 }} />
-            </LineChart>
-        </ResponsiveContainer>
+        <div style={{ width: '100%', height: '30vh' }}>
+            <ResponsiveContainer width="100%" height="100%">
+                <LineChart data={data}>
+                    <Line type="monotone" dataKey="heartRate" stroke="#8884d8" dot={false} />
+                </LineChart>
+            </ResponsiveContainer>
+        </div>
     );
 };
 
